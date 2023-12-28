4d09ba57dec1442a3b6e0877e831653f46e109b7
Chaher <chaher.croissance.public@gmail.com>
2023-12-28 14:46:37 +0100

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Chaher Machhour on 28/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
