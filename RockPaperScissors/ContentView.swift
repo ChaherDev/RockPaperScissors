f1af5271830aefb1290550616c2f77d7d6930772
Chaher Machhour <chaher.croissance.public@gmail.com>
2024-10-07 17:23:10 +0200

    Variables d'etat pour gerer le deroulement du jeu

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,12 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let moves = ["Rock", "Paper", "Scissors"]
+    
+    @State private var appMove = 0 // Le choix de l'app qui changera au hazard
+    @State private var shouldWin = true // Si le joueur doit gagner ou perdre
+    @State private var score = 0 // Le score du joueur
+    @State private var questiionCount = 0 // Le nombre de tours joués
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Score : \(score)")
+                .font(.largeTitle)
+            
+            Text("L'App a joué : \(moves[appMove])")
+                .font(.title)
+            
+            Text(shouldWin ? "Vous devez gagner" : "Vous devez perdre")
+                .font(.title2)
+            
+            // Les boutons
         }
         .padding()
     }
